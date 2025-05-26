6bc91d314c58a648f9816e03ad1ae93346a11f58
Harshaharika7 <138356696+Harshaharika7@users.noreply.github.com>
2025-05-26 20:17:22 +0530

    Update scope.py

--- scope.py
+++ scope.py
@@ -10,7 +10,7 @@ class Difference:
 # End of Difference class
 
 _ = input()
-a = [int(e) for e in input().split(' ')]
+a = [int(e) for e in input().split()]
 
 d = Difference(a)
 d.computeDifference()
